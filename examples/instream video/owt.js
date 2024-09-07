83f8d99c160955491c191e4cb5795c97365ec6e7
pm-azhar-mulla <azhar@L1119.local>
2024-09-07 14:25:41 +0530

    Updated profile and version

--- examples/instream video/owt.js
+++ examples/instream video/owt.js
@@ -23412,9 +23412,9 @@
     /***/ (function(module, exports) {
     
     exports.pwt = {
-        pid: "61738",
+        pid: "5629",
         gcv: "172",
-        pdvid: "1",
+        pdvid: "10",
         pubid: "5890",
         dataURL: "t.pubmatic.com/wl?",
         winURL: "t.pubmatic.com/wt?",
